123311315eda4e6d45f122936175ffc6c8fb56fe
Laura Gupta <95309002+lauragupta@users.noreply.github.com>
2022-01-30 12:46:44 -0800

    add port, get route and app.listen set up

--- server.js
+++ server.js
@@ -2,9 +2,26 @@ const express = require('express');
 const path = require('path');
 const fs = require('fs');
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 const app = express();
 
+//Middleware for JSON
+app.use(express.json());
+
+
+//GET route for homepage
 app.get('/', (req, res) => 
- )
+    res.sendFile(path.join(__dirname, '/public/index.html'))
+);
+
+//GET route for note page
+app.get('/api/notes',(req, res) =>
+    res.sendFile(path.join(__dirname, '/public/notes.html'))
+);
+
+app.listen(PORT, () =>
+    console.log(`App is listening at http://localhost:${PORT}`)
+);
+
+module.exports = app;
