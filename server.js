ade5b3e82e04e229a1848f9ec0715d7b44eb2cd0
Laura Gupta <95309002+lauragupta@users.noreply.github.com>
2022-01-30 23:22:01 -0800

    add middleware and GET paths

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const path = require('path');
+const textData = require('./db/db.json')
+const uniqid = require('uniqid');
+const { title } = require('process');
 // const fs = require('fs');
 // import db from "./Develop/db/db.json";
 
@@ -9,17 +12,21 @@ const app = express();
 
 //Middleware for JSON
 app.use(express.json());
+app.use(express.urlencoded({extended: true}))
 
+app.use(express.static('public'));
 
 //GET route for homepage
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '/public/index.html'))
 });
 
-//GET route for note page
+//GET route for note page - notes
 app.get('/notes',(req, res) => {
     res.sendFile(path.join(__dirname, '/public/notes.html'))
 });
+//Get route for note page - title
+app.get('/api/notes', (req, res) => res.json(textData));
 
 app.listen(PORT, () =>
     console.log(`App is listening at http://localhost:${PORT}`)
