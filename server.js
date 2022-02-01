98f34d3a303b1722c12cddf4a70f70931f111b53
Laura Gupta <95309002+lauragupta@users.noreply.github.com>
2022-01-31 20:53:25 -0800

    add fs read, write, and append file

--- server.js
+++ server.js
@@ -2,8 +2,9 @@ const express = require('express');
 const path = require('path');
 const textData = require('./db/db.json')
 const uniqid = require('uniqid');
-const { title } = require('process');
-// const fs = require('fs');
+// const { title } = require('process');
+const fs = require('fs');
+const util = require('util');
 // import db from "./Develop/db/db.json";
 
 const PORT = 3001;
@@ -23,13 +24,57 @@ app.get('/', (req, res) => {
 
 //GET route for note page - notes
 app.get('/notes',(req, res) => {
-    res.sendFile(path.join(__dirname, '/public/notes.html'))
+    res.sendFile(path.join(__dirname, '/public/notes.html'));
+    readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
 });
+
 //Get route for note page - title
 app.get('/api/notes', (req, res) => res.json(textData));
+
+//fs.readFile - promisified version
+readFromFile = util.promisify(fs.readFile);
+
+//writeToFile
+const writeToFile = (destination, content) =>
+    fs.writeFile(destination, JSON.stringify(content), (err) =>
+    err? console.error(err) :console.info(`Note written to ${destination}`)
+);
+
+//defining readAndAppend
+const readAndAppend = (content, file) => {
+    fs.readFile(file, 'utf8', (err, data) => {
+        if(err) {
+            console.error(err);
+        } else {
+            const parsedData = JSON.parse(data);
+            parsedData.push(content);
+            writeToFile(file, parsedData);
+        }
+    });
+};
+
+//Post route for note and title
+app.post('/api/notes', (req, res) => {
+    console.info(`${req.method} received request to add new note`);
+
+    const { title, text } = req.body
+
+    if (req.body) {
+        const newNote = {
+            title, 
+            text,
+            noteId: uniqid()
+        };
+
+        readAndAppend(newNote, './db/db.json');
+
+        res.json(`Your note was added successfully`);
+    } else {
+        res.error('Error in adding note');
+    }
+});
 
 app.listen(PORT, () =>
     console.log(`App is listening at http://localhost:${PORT}`)
 );
 
-module.exports = app;
